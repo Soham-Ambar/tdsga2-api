3e831409fb3e9d575b9131fd832e6997bc846fb7
soham <soham.ambar23@vit.edu>
2026-06-28 18:17:29 +0530

    Fix Q2 JWT verification

--- routes/q2_verify.py
+++ routes/q2_verify.py
@@ -6,15 +6,17 @@ import jwt
 router = APIRouter()
 
 ISSUER = "https://idp.exam.local"
+AUDIENCE = "tds-92lshwvw.apps.exam.local"
 
-AUDIENCE = "tds-abc12345.apps.exam.local"
-
-PUBLIC_KEY = """
------BEGIN PUBLIC KEY-----
-MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQE...
-...
------END PUBLIC KEY-----
-"""
+PUBLIC_KEY = """-----BEGIN PUBLIC KEY-----
+MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA2okOHspNjgA+2rTLbeuY
+cxiP/hG8C6Sb9iwg3yiLAA4HCnpITcbWCSelbvbYGuc3EbNy4xFyf5Cbj5DHJMID
+EkryOgyd2giIIIBOUBj8S63uGcnRpOBh9NFatfNwheKuzsPuVNldu6A9cNteNpXc
+WyJjG2axVfmq7i6SuKr1JoWYG7xTTAvKPujSl4OtsQfO3h5NepzdfXpr28oNnzfW
+ed+zclR6BcmNNo/WVfJ4xyCLSf0BCOgdTgW6PdaChd1l9VDetJZVEgC5tkyvXsfI
+SI6iyrYbKR0NEBSqq4XkadEjsCs4F1RncsS4LlgniT7GlkL9Mce3b0wGLs9/7ZIX
+dQIDAQAB
+-----END PUBLIC KEY-----"""
 
 
 class TokenRequest(BaseModel):
@@ -30,6 +32,7 @@ def verify_token(request: TokenRequest):
             algorithms=["RS256"],
             issuer=ISSUER,
             audience=AUDIENCE,
+            options={"require": ["exp", "iss", "aud"]},
         )
 
         return {
@@ -42,5 +45,5 @@ def verify_token(request: TokenRequest):
     except Exception:
         return JSONResponse(
             status_code=401,
-            content={"valid": False}
+            content={"valid": False},
         )
